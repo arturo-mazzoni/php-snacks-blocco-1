cb32450926502349470ef2fcf454250150d3a343
arturo <arturomazzoni@gmail.com>
2021-02-25 22:03:33 +0100

    snack 2 complete

--- index.php
+++ index.php
@@ -1,34 +1,11 @@
 <?php
-  $matches = [
-    [
-      "casa" => "Fortitudo",
-      "ospite" => "Virtus",
-      "puntiCasa" => "54",
-      "puntiOspite" => "43"
-    ],
-    [
-      "casa" => "Olimpia Milano",
-      "ospite" => "Cantu",
-      "puntiCasa" => "55",
-      "puntiOspite" => "60",
-    ]
-  ];
+$name = $_GET["name"];
+$email = $_GET["email"];
+$age = $_GET["age"];
+
+if (strlen($name) > 3 && !empty($email) && strpos($email, "@") && strpos($email, ".") && is_numeric($age)){
+  echo "Accesso riuscito";
+} else {
+  echo "Accesso negato";
+}
 ?>
-
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <meta http-equiv="X-UA-Compatible" content="ie=edge">
-  <title>Document</title>
-</head>
-<body>
-
-  <?php for ($i = 0; $i < count($matches); $i++) { ?>
-    <h1> <?php echo $matches[$i]["casa"] . " - " .$matches[$i]["ospite"] ."  |  " .$matches[$i]["puntiCasa"] . " - " .$matches[$i]["puntiOspite"] ?> </h1>
-  <?php } ?>
-
-
-</body>
-</html>
---
+++ index2.php
@@ -0,0 +1,36 @@
+<!-- ***snack 1*** -->
+
+<?php
+  $matches = [
+    [
+      "casa" => "Fortitudo",
+      "ospite" => "Virtus",
+      "puntiCasa" => "54",
+      "puntiOspite" => "43"
+    ],
+    [
+      "casa" => "Olimpia Milano",
+      "ospite" => "Cantu",
+      "puntiCasa" => "55",
+      "puntiOspite" => "60",
+    ]
+  ];
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <title>Document</title>
+</head>
+<body>
+
+  <?php for ($i = 0; $i < count($matches); $i++) { ?>
+    <h1> <?php echo $matches[$i]["casa"] . " - " .$matches[$i]["ospite"] ."  |  " .$matches[$i]["puntiCasa"] . " - " .$matches[$i]["puntiOspite"] ?> </h1>
+  <?php } ?>
+
+
+</body>
+</html>
